c3b4b00b8218a0a4fb6e7bfe30012c42521f4e92
Brandon Elam Barker <beb82@cornell.edu>
2016-08-31 09:18:30 -0400

    CDR-140: Factored out services from rdb module

---
+++ services-core/src/main/java/edu/ncrn/cornell/util/DDIHandle.java
@@ -0,0 +1,15 @@
+package edu.ncrn.cornell.util;
+
+/**
+ * DDI-specific extensions for an XMLHandle
+ *
+ * @author Brandon Barker (2016)
+ */
+public class DDIHandle extends XMLHandle {
+
+    public DDIHandle(String x, String schemaUrl) {
+        super(x, schemaUrl);
+    }
+
+
+}
